ccaa6e67fa78f33cf51738e62f436b6d5e149add
Caio Guilherme <caioguilherme_10@hotmail.com>
2023-06-18 17:04:17 -0300

    Funcionalidade criar mais uma coluna testada.

--- index.js
+++ index.js
@@ -3,6 +3,11 @@
     document.getElementById("testeBut1").click()
     })
 
+    //  Botão para testar a funcionalidade "criar mais uma coluna"
+    document.getElementById("teste2").addEventListener("click", ()=>{
+    document.getElementById("testeBut2").click()
+})
+
 // CRIAR MAIS UMA LINHA
         function addLine(event){
         var ct = event.parentElement
@@ -11,6 +16,15 @@
         ct.prepend(inp)
     }
 
+//  CRIAR MAIS UMA COLUNA
+    function addList(event){
+    var list = event
+    var addList = document.createElement("div")
+    addList.innerHTML='<input class="list-title" onfocus="addInput(this)" onfocusout="removeFocus(this)"></input><img src="imagens/lixeira.png" style="height: 16px;" onclick="removeLinha(this)" id="deletaList"><ul class="list-items"><div class="inputapaga"><input type="text" readonly="readonly" placeholder="Digite aqui..." onfocus="addInput(this)" onfocusout="removeFocus(this)"></input><img src="imagens/lixeira.png" style="height: 16px;" onclick="removeLinha(this)" ></div><button class="add-card-btn btn" onclick="addLine(this)">Add a card</button></ul>'
+    addList.className="list"
+    list.parentElement.insertBefore(addList, list)
+} 
+ 
 //  Permite a edição do input
     function addInput(event){
         event.removeAttribute("readonly")
